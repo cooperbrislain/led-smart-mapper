a00dd98b3e6b88aa8f7072abb30f11a43e674106
Cooper Brislain <cbrislain@gmail.com>
2018-04-20 23:49:35 -0700

    try this

--- mqtt_ledstrip.ino
+++ mqtt_ledstrip.ino
@@ -46,7 +46,7 @@ class Light {
         bool _onoff;
         String _name;
         int _count;
-        int *_prog();
+        int (*_prog)();
         void add_to_homebridge();
         void subscribe(String);
 
